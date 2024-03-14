b3d4db7f6e73f114b1c41e1d835247f94851c755
RuviniDharmasena <126440617+RuviniDharmasena@users.noreply.github.com>
2024-03-15 00:51:37 +1100

    pause and resume button change icon

--- Assets/Scripts/PauseGame.cs
+++ Assets/Scripts/PauseGame.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public GameObject pauseIcon;
+    public GameObject playIcon;
     bool paused = false;
+    
 
     public void pausegame()
     {
         if (paused)
         {
             Time.timeScale = 1;
+             pauseIcon.SetActive(true);
+             playIcon.SetActive(false);
             paused = false;
         }
         else{
             Time.timeScale = 0;
+            pauseIcon.SetActive(false);
+             playIcon.SetActive(true);
             paused = true;
         }
     }
